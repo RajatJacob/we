b152f0a209f06df5dad7d1e5f94ff67e85488804
Vansikaa <vansibanu@gmail.com>
2019-12-18 10:47:14 +0530

    Added CSS

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import './Footer.css';
 
-
 class Footer extends React.Component {
     constructor(props)
     {
         super(props)
         this.state= 
         {
-            links: 
+            links: this.props.links==undefined ?
         [
             {name:"Code Of Conduct",url:"/"},
             {name:"Privacy Policy",url:"/"},
             {name:"Feedback",url:"/"},
             {name:"Contact",url:"/"}
-        ]
+        ] : this.props.links
         }
 
     }
+    render()
+    {
+        return (
+        <div className="Footer">
+            <ul id="links" >
+            {
+                this.state.links.map((f) => {
+                   return(
+                    <a href={f.url}>
+                        <li>
+                            {f.name}  
+                        </li>
+                    </a>
+                    )
+                })
+            }
+            </ul>
+        </div> 
+        );
+    }
+
 }
+export default Footer;
