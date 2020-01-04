2fd48c63279c12d2ba6904d13e63e392051d1124
Rajat Jacob <rajatjacob@gmail.com>
2020-01-04 10:48:56 +0530

    React Router Working

--- src/global/App.js
+++ src/global/App.js
@@ -1,17 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
 import Card from '../components/Card/Card';
 import Footer from '../components/Footer/Footer';
 
 function App() {
 	return (
+		<Router>
 		<div className="App">
-			<NavigationBar></NavigationBar>
-			<Card>
-				<h1>Card</h1>
-			</Card>
-			<Footer></Footer>
+			<NavigationBar />
+			<div className="Content">
+				<Switch>
+					<Route exact path="/">
+						Home
+					</Route>
+					<Route exact path="/who-we-are">
+						Who We Are
+					</Route>
+					<Route exact path="/opportunities">
+						Opportunities
+				  	</Route>
+					<Route exact path="/join-us">
+						Join Us
+				  	</Route>
+					<Route exact path="/support">
+						Support
+				  	</Route>
+					<Route exact path="/community">
+						Community
+				  	</Route>
+				</Switch>
+				
+				<Footer />
+			</div>
 		</div>
+		</Router>
 	);
 }
 
