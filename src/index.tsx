c03505abf3cb41b27820e8c097146ccb31800bb2
Rajat Jacob <rajatjacob@gmail.com>
2020-01-16 21:42:27 +0530

    Moved common files to the root directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './global/App';
-import * as serviceWorker from './global/serviceWorker';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
