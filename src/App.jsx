83e14e1070430321a8d0f66d8022eed09e1448a6
Rajat Jacob <rajatjacob@gmail.com>
2020-01-18 09:15:28 +0530

    Fixed indentation issues in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.scss';
 
 export default class App extends React.Component {
-  render() {
-	return (
-		<div className="App">
-			<h1>WE</h1>
-		</div>
-	);
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1>WE</h1>
+			</div>
+		);
+	}
 }
