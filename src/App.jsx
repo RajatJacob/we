4d5fd85dfbaecedd1741b28d21f47b4310a70296
Rajat Jacob <rajatjacob@gmail.com>
2020-01-18 09:41:22 +0530

    Created Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.scss';
+import Card from './components/Card';
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<div className="App">
-				<h1>WE</h1>
+				<Card>
+					<h1>WE</h1>
+				</Card>
 			</div>
 		);
 	}
---
+++ src/components/Card/README.md
@@ -0,0 +1,15 @@
+# Card
+
+A component to show a card.
+
+## Usage
+
+```html
+<Card>
+    Inner stuff
+</Card>
+```
+
+# Properties
+
+- ```noContainer```: To remove the default container for the child elements.
