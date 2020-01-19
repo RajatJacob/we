640777b4b9849d2024b06eaa67601da94beea02f
Rajat Jacob <rajatjacob@gmail.com>
2020-01-19 22:16:54 +0530

    Installed and initialised react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.scss';
-import Card from './components/Card';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Container from './components/Container';
 
 export default class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<Card>
-					<h1>WE</h1>
-				</Card>
-			</div>
-		);
-	}
+  render() {
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Container>
+							<h1>Home</h1>
+						</Container>
+					</Route>
+					<Route path="*">
+						<Container>
+							<h1>404</h1>
+							Page Not Found!
+						</Container>
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
+  }
 }
