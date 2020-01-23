7e53853468da0c8bc06f45bfee975d3a432b042d
vansikaa <vansibanu@gmail.com>
2020-01-23 14:21:03 +0530

    Update index.jsx

--- src/pages/UserProfile/index.jsx
+++ src/pages/UserProfile/index.jsx
@@ -3,23 +3,22 @@ import Card from '../../components/Card';
 import Container from '../../components/Container';
 import './style.scss';
 import GridContainer from '../../components/GridContainer';
-//import ImageUploader from 'react-images-upload';
 
 export default class UserProfile extends React.Component {
-	render() {
-		return (
-			<div className="UserProfile">
-				<Card noContainer>
-					<GridContainer>
-						<Container>
-							<h1>User Profile</h1>
-						</Container>
-						<Container>
-							<h1>Followers</h1>
-						</Container>
-					</GridContainer>
-				</Card>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div className="UserProfile">
+                <Card noContainer>
+                    <GridContainer>
+                        <Container>
+                            <h1>User Profile</h1>
+                        </Container>
+                        <Container>
+                            <h3>Followers</h3>
+                        </Container>
+                    </GridContainer>
+                </Card>
+            </div>
+        )
+    }
 }
