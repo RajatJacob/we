c2463fd14dc964099f388e51a06eb6c81dbdaba1
Rajat Jacob <rajatjacob@gmail.com>
2020-01-23 19:03:32 +0530

    Basic Alerts are done

---
+++ src/components/Alert/README.md
@@ -0,0 +1,15 @@
+# Alert
+
+A component to display an alert.
+
+## Properties
+
+* ` title ` : The alert's title.(Optional)
+* ` message ` : The alert message.
+* `type` : A string that describes the type of alert. The accepted types are:
+  + ` default ` (Gray)
+  + ` success ` (Green)
+  + ` info ` (Blue)
+  + ` warning ` (Yellow)
+  + ` danger ` (Red)
+
--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -4,6 +4,7 @@ import Container from '../../components/Container';
 import Input from '../../components/Input';
 import GridContainer from '../../components/GridContainer';
 import { FirebaseContext } from '../../contexts/FirebaseContext';
+import Alert from '../../components/Alert';
 
 export default class LoginPage extends React.Component {
 	static contextType = FirebaseContext;
@@ -18,9 +19,23 @@ export default class LoginPage extends React.Component {
 	submit = e => {
 		const { auth } = this.context;
 		e.preventDefault()
+		this.setState({
+			alert: {
+				type: "success",
+				message: "Logging in..."
+			}
+		})
 		auth.signInWithEmailAndPassword(this.state.email, this.state.password)
-			.then(() => { this.setState({ error: null }) })
-			.catch((error) => { this.setState({ error: error }) })
+			.then(() => { this.setState({ alert: null }) })
+			.catch((error) => {
+				this.setState({
+					alert: {
+						type: "danger",
+						title: error.code,
+						message: error.message
+					}
+				})
+			})
 		e.target.reset()
 		this.setState({ email: "", password: "" })
 	}
@@ -42,16 +57,15 @@ export default class LoginPage extends React.Component {
 								<Input label="E-mail" icon="@" onChange={e => this.setState({ email: e.target.value })} />
 								<Input label="Password" type="password"
 									icon="*" onChange={e => this.setState({ password: e.target.value })} />
-								{
-									this.state.error ?
-										<Card>
-											<strong>{this.state.error.code}</strong>
-											<p>{this.state.error.message}</p>
-										</Card> :
-										null
-								}
 								<Input label="Login" type="submit" />
 							</form>
+							{
+								this.state.alert ?
+									<Alert type={this.state.alert.type} title={this.state.alert.title}>
+										{this.state.alert.message}
+									</Alert> :
+									null
+							}
 						</Container>
 					</GridContainer>
 				</Card>
