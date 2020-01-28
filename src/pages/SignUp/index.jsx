0e32a715117945993b16cf53ab194806db37272f
vansikaa <vansibanu@gmail.com>
2020-01-28 09:30:59 +0530

    Basi auth for SignUp done.

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -13,13 +13,16 @@ export default class SignUp extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            name: "",
+            username: "",
             email: "",
-            password: ""
+            password: "",
+            confirmPassword: ""
         }
     }
 
     submit = e => {
-        const { auth } = this.context;
+        const { auth, firestore, getUserRefByUsername } = this.context;
         e.preventDefault()
         this.setState({
             alert: {
@@ -27,8 +30,30 @@ export default class SignUp extends React.Component {
                 message: "Signing in..."
             }
         })
-        auth.signInWithEmailAndPassword(this.state.email, this.state.password)
-            .then(() => { this.setState({ alert: null }) })
+        auth.createUserWithEmailAndPassword(this.state.email, this.state.password)
+            .then(() => {
+                this.setState({ alert: null })
+                getUserRefByUsername(this.state.username).get().then(
+                    snapshot => {
+                        if (snapshot.size !== 0) {
+                            this.setState({
+                                alert: {
+                                    type: "danger",
+                                    title: "User exists",
+                                    message: "Please use a different username"
+                                },
+                                username: ""
+                            })
+                        }
+                        else {
+                            firestore.collection('users').doc(auth.currentUser.user.uid).set({
+                                name: this.state.name,
+                                username: this.state.username
+                            })
+                        }
+                    }
+                )
+            })
             .catch((error) => {
                 this.setState({
                     alert: {
@@ -39,7 +64,10 @@ export default class SignUp extends React.Component {
                 })
             })
             .finally(() => {
-                this.setState({ email: "", password: "" })
+                this.setState({
+                    password: "",
+                    confirmPassword: ""
+                })
             })
     }
 
@@ -57,13 +85,13 @@ export default class SignUp extends React.Component {
                         </Container>
                         <Container>
                             <form onSubmit={this.submit}>
-                                <Input label="Name" icon={<FontAwesomeIcon icon={faUser} onChange={e => this.setState({ name: e.target.value })} />} />
-                                <Input label="Username" icon={<FontAwesomeIcon icon={faGhost} />} onChange={e => this.setState({ username: e.target.value })} />
-                                <Input label="E-mail" icon={<FontAwesomeIcon icon={faEnvelope} />} onChange={e => this.setState({ email: e.target.value })} />
+                                <Input label="Name" icon={<FontAwesomeIcon icon={faUser} onChange={e => this.setState({ name: e.target.value })} value={this.state.name} />} />
+                                <Input label="Username" icon={<FontAwesomeIcon icon={faGhost} />} onChange={e => this.setState({ username: e.target.value })} value={this.state.username} />
+                                <Input label="E-mail" icon={<FontAwesomeIcon icon={faEnvelope} />} onChange={e => this.setState({ email: e.target.value })} value={this.state.email} />
                                 <Input label="Password" type="password"
-                                    icon={<FontAwesomeIcon icon={faLock} />} onChange={e => this.setState({ password: e.target.value })} />
+                                    icon={<FontAwesomeIcon icon={faLock} />} onChange={e => this.setState({ password: e.target.value })} value={this.state.password} />
                                 <Input label="Confirm Password" type="password"
-                                    icon={<FontAwesomeIcon icon={faLock} />} onChange={e => this.setState({ confirmPassword: e.target.value })} />
+                                    icon={<FontAwesomeIcon icon={faLock} />} onChange={e => this.setState({ confirmPassword: e.target.value })} value={this.state.confirmPassword} />
                                 {
                                     this.state.alert ?
                                         <Alert type={this.state.alert.type} title={this.state.alert.title}>
