9f1da520c5becf5f076b88d97e6ade811f4b8116
vansikaa <vansibanu@gmail.com>
2020-01-29 09:53:27 +0530

    Grid Container introduced in Donation

--- src/pages/Donation/index.jsx
+++ src/pages/Donation/index.jsx
@@ -3,17 +3,27 @@ import Card from '../../components/Card';
 import Input from '../../components/Input';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faEnvelope, faMobile, faWallet } from '@fortawesome/free-solid-svg-icons';
+import GridContainer from '../../components/GridContainer';
+import Container from '../../components/Container';
 class Donation extends React.Component {
     render() {
         return (
             <div classname="Donation">
                 <Card>
-                    <h1></h1>Donation
-                    <Input label="Name" icon={<FontAwesomeIcon icon={faUser} />} />
-                    <Input label="E-mail" icon={<FontAwesomeIcon icon={faEnvelope} />} />
-                    <Input label="Phone Number" icon={<FontAwesomeIcon icon={faMobile} />} />
-                    <Input label="Amount" icon={<FontAwesomeIcon icon={faWallet} />} />
-                    <Input label="Donate" type="Submit" />
+
+
+                    <GridContainer>
+                        <Container>
+                            <h1> Donation</h1>
+                        </Container>
+                        <Container>
+                            <Input label="Name" icon={<FontAwesomeIcon icon={faUser} />} />
+                            <Input label="E-mail" icon={<FontAwesomeIcon icon={faEnvelope} />} />
+                            <Input label="Phone Number" icon={<FontAwesomeIcon icon={faMobile} />} />
+                            <Input label="Amount" icon={<FontAwesomeIcon icon={faWallet} />} />
+                            <Input label="Donate" type="Submit" />
+                        </Container>
+                    </GridContainer>
                 </Card>
             </div>)
     }
