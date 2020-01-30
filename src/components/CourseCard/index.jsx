da137076b50f928e4ce26d8b7d69cc7e59ac6c3c
Rajat Jacob <rajatjacob@gmail.com>
2020-01-30 13:10:49 +0530

    Linted all the code :)

--- src/components/CourseCard/index.jsx
+++ src/components/CourseCard/index.jsx
@@ -1,46 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Input from '../../components/Input';
 import './style.scss';
-import Container from '../../components/Container';
 import img10 from '../../images/calendar.png';
 
-const Cards = props =>{
-	return(
+const Cards = props => {
+	return (
 		<container>
-		<div>
+			<div>
 
-		<div className="card">
-			<div className='card-flow'>
-				<div className='card-flow-front'>
-					<img src={props.imgsrc} alt="props.name" className="card-img"/>
-					
-						<h5 className="title">{props.title}</h5>
-					
+				<div className="card">
+					<div className='card-flow'>
+						<div className='card-flow-front'>
+							<img src={props.imgsrc} alt="props.name" className="card-img" />
+
+							<h5 className="title">{props.title}</h5>
+
+						</div>
+						<div className='card-body'>
+							<div className="color"> </div>
+							<div className="body">
+								<p className="content">{props.content}</p>
+							</div>
+							<div className="weeks">
+								<img className="weekimage" src={img10} alt="props.name" />
+								<p className="weeklabel">{props.label}</p>
+							</div>
+							<footer>
+								<p><button type="button">Learn</button></p>
+							</footer>
+						</div>
+					</div>
 				</div>
-					<div className='card-body'>
-					<div className="color"> </div>
-						<div className="body">
-							<p className="content">{props.content}</p>
-						</div>
-						<div className="weeks">
-							<img className="weekimage" src={img10} alt="props.name" /> 
-							<p className="weeklabel">{props.label}</p>
-						</div>
-						<footer>
-							<p><button type="button">Learn</button></p>
-						</footer>
-					</div>
-					</div>
+				<br />
+				<br />
 			</div>
-		<br/>
-		<br/>
-		</div>
 		</container>
-				);
-			}
+	);
+}
 
-			export default Cards;
-
-
-			// <Input className="icon" label={props.label} icon={ <FontAwesomeIcon icon={faCalendar}/>}/>
+export default Cards;
