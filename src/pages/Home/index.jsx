83f64ee26004c4c2c21517dfc4e8b11f1f34c2ba
vansikaa <vansibanu@gmail.com>
2020-01-31 23:34:48 +0530

    3rd background image set

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,28 +3,29 @@ import './style.scss';
 import Container from '../../components/Container';
 
 class Home extends React.Component {
-	render() {
-		return (
-			<div className="Home">
-				<div className="background banner" id="top">
-					WE
+    render() {
+        return (
+            <div className="Home">
+                <div className="background banner" id="top">
+                    WE
                 </div>
-				<div className="content">
-					<Container>
-						<h1>Our Vision</h1>
-						A world where every woman has a platform to voice her opinion.
+                <div className="content">
+                    <Container>
+                        <h1>Our Vision</h1>
+                        A world where every woman has a platform to voice her opinion.
 					</Container>
-				</div>
-				<div className="background" id="rural" />
-				<div className="content">
-					<Container>
-						<h1>Our Mission</h1>
-						To provide a safe place to allow women to express their views.
+                </div>
+                <div className="background" id="rural" />
+                <div className="content">
+                    <Container>
+                        <h1>Our Mission</h1>
+                        To provide a safe place to allow women to express their views.
 					</Container>
-				</div>
-			</div>
-		);
-	}
+                </div>
+                <div className="background" id="bottom" />
+            </div>
+        );
+    }
 
 }
 export default Home;
