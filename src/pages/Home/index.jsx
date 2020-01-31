851067fd392f690d9554b37b8680e0c4bffba216
Rajat Jacob <rajatjacob@gmail.com>
2020-01-31 14:03:22 +0530

    Basic Home Page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,34 +1,30 @@
 import React from 'react';
 import './style.scss';
+import Container from '../../components/Container';
 
 class Home extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state =
-        {
-            links: this.props.links === undefined ?
-                [
-                    { name: "Who We Are", url: "/who-we-are" },
-                    { name: "Join Us", url: "/join-us" },
-                    { name: "WE Offer", url: "/we-offer" },
-                    { name: "Privacy Policy", url: "/privacy-policy" },
-                    { name: "Code Of Conduct ", url: "/code-of-conduct" },
-                ] : this.props.links
-        }
-
-    }
-    render() {
-        return (
-            <div className="Home">
-                <div className="background banner">
-                    WE
+	render() {
+		return (
+			<div className="Home">
+				<div className="background banner" id="top">
+					WE
                 </div>
-                <div className="content">
-                    A world where every woman is able to use her voice and make her own decisions.
-                </div>
-            </div>
-        );
-    }
+				<div className="content">
+					<Container>
+						<h1>Our Vision</h1>
+						A world where every woman has a platform to voice her opinion.
+					</Container>
+				</div>
+				<div className="background" id="rural" />
+				<div className="content">
+					<Container>
+						<h1>Our Mission</h1>
+						To provide a safe place to allow women to express their views.
+					</Container>
+				</div>
+			</div>
+		);
+	}
 
 }
 export default Home;
