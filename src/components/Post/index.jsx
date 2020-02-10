aed6e16c8afda85262d6e67501613e4cf17fbb26
vansikaa <vansibanu@gmail.com>
2020-02-10 15:44:08 +0530

    Create index.jsx

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../Card';
+import Input from '../Input';
+
+export default class Post extends React.Component {
+    render() {
+        return (
+            <Card>
+                <div>
+
+                </div>
+            </Card>
+        )
+    }
+}
