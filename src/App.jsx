71bd4053d41a20d6269e54ef2ce6b83de52488c6
sanskaraggarwal <sanskaraggarwal1@gmail.com>
2020-02-15 00:36:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -20,3 +21,28 @@ export default class App extends React.Component {
 		);
 	}
 }
+=======
+
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NavigationBar from './components/NavigationBar';
+import Routes from './Routes';
+import FirebaseContextProvider from './contexts/FirebaseContext';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				<Router>
+					<FirebaseContextProvider>
+						<NavigationBar />
+						<Routes />
+					</FirebaseContextProvider>
+					
+				</Router>
+			</div >
+		);
+	}
+}
+>>>>>>> Stashed changes
