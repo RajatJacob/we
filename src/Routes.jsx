3decfc289e08b8be8adb1395daae5b25fcf5c2ff
sanskaraggarwal <sanskaraggarwal1@gmail.com>
2020-02-15 00:37:02 +0530

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { FirebaseContext } from './contexts/FirebaseContext';
@@ -33,4 +34,51 @@ export default class Routes extends React.Component {
 			</div>
 		);
 	}
+=======
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { FirebaseContext } from './contexts/FirebaseContext';
+import Login from './pages/Login';
+import UserProfile from './pages/UserProfile';
+import Card from './components/Card';
+import SignUp from './pages/SignUp';
+import Logout from './pages/Logout';
+import ForgotPassword from './pages/ForgotPassword';
+import VocationalCourses from './pages/VocationalCourses';
+import Home from './pages/Home';
+import Feedback from './components/Feedback';
+import Resume from './components/Resume';
+
+export default class Routes extends React.Component {
+	static contextType = FirebaseContext
+	render() {
+		return (
+			<div className="Content">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/signup" component={SignUp} />
+					<Route exact path="/login" component={Login} />
+					<Route path="/user/:username" component={UserProfile} />
+					<Route exact path="/logout" component={Logout} />
+					<Route exact path="/forgotpassword" component={ForgotPassword} />
+					<Route exact path="/courses" component={VocationalCourses} />
+					<Route exact path="/Resume">
+						<Resume />
+					</Route>
+					<Route exact path="/Feedback">
+						<Feedback />
+					</Route>
+					<Route path="*">
+						<Card>
+							<h1>404</h1>
+							Page Not Found!
+						</Card>
+					</Route>
+				</Switch>
+			</div>
+		);
+	}
+
+>>>>>>> Stashed changes
 }
