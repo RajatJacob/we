b6b5f0831369d71f5f40bc08225371bd767e20cc
vansikaa <vansibanu@gmail.com>
2020-03-05 14:05:41 +0530

    Added to navigation bar

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -3,13 +3,17 @@ import './style.scss';
 import Card from '../../components/Card';
 import Container from '../../components/Container';
 import Input from '../../components/Input';
+import { FirebaseContext } from '../../contexts/FirebaseContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Alert from '../../components/Alert';
 
 
 export default class Search extends React.Component {
+
+    static contextType = FirebaseContext;
     constructor(props) {
+
         super(props)
         this.state = {
             search: "",
@@ -17,9 +21,20 @@ export default class Search extends React.Component {
             proceed: false
         }
     }
+
+    search = text => {
+        const { firestore } = this.context
+        firestore
+            .collection("users")
+            .where("username", ">=", text)
+            .where("username", "<=", text + "")
+    }
+
     render() {
         return (
             <div className="Search">
+                <div className="background">
+                </div>
                 <Card>
                     <Container>
                         <Input label="Search" icon={<FontAwesomeIcon icon={faSearch} />} onChange={e => this.setState({ search: e.target.value })} value={this.state.search} required />
