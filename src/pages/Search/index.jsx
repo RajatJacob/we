d6bc7e9498a2a94d6e394c32ae5c41818f31d20d
vansikaa <vansibanu@gmail.com>
2020-03-05 12:27:55 +0530

    Basic textbox displayed

---
+++ src/pages/Search/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './style.scss';
+import Card from '../../components/Card';
+import Input from '../../components/Input';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Alert from '../../components/Alert';
+
+
+export default class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            search: "",
+            alert: null,
+            proceed: false
+        }
+    }
+    render() {
+        return (
+            <div className="Search">
+                <Card>
+                    <Input label="Search" icon={<FontAwesomeIcon icon={faSearch} />} onChange={e => this.setState({ search: e.target.value })} value={this.state.search} required />
+                    {
+                        this.state.alert ?
+                            <Alert type={this.state.alert.type} title={this.state.alert.title}>
+                                {this.state.alert.message}
+                            </Alert> :
+                            null
+                    }
+                </Card>
+
+            </div>
+        )
+    }
+}
