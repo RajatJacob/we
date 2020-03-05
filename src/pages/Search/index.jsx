32dc7d2850259bc7fe715bb7c396088d7e05a34d
vansikaa <vansibanu@gmail.com>
2020-03-05 14:53:31 +0530

    Fixed search

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './style.scss';
 import Card from '../../components/Card';
-import Container from '../../components/Container';
 import Input from '../../components/Input';
 import { FirebaseContext } from '../../contexts/FirebaseContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import Alert from '../../components/Alert';
 import UserList from '../../components/UserList';
 
 export default class Search extends React.Component {
@@ -17,21 +15,17 @@ export default class Search extends React.Component {
         super(props)
         this.state = {
             search: "",
-            result: [],
-            alert: null,
-            proceed: false
+            result: []
         }
     }
 
-    search = e => {
-        e.preventDefault();
-        this.setState({ search: e.target.value, result: [] })
-        const text = e.target.value
+    search = text => {
         const { firestore } = this.context
         firestore
             .collection("users")
             .where("username", ">=", text)
             .where("username", "<=", text + '\uf8ff')
+            .orderBy("username")
             .get()
             .then(
                 snapshot => {
@@ -48,26 +42,33 @@ export default class Search extends React.Component {
 
     }
 
+    handleSearch = e => {
+        e.preventDefault();
+        this.setState({ search: e.target.value, result: [] })
+        const text = e.target.value
+        this.search(text)
+    }
+
+    componentDidMount() {
+        this.search("")
+    }
+
     render() {
         return (
             <div className="Search">
                 <div className="background">
                 </div>
                 <Card>
-                    <Container>
-                        <Input label="Search" icon={<FontAwesomeIcon icon={faSearch} />} onChange={this.search
-                        } value={this.state.search} required />
-                        {
-                            this.state.alert ?
-                                <Alert type={this.state.alert.type} title={this.state.alert.title}>
-                                    {this.state.alert.message}
-                                </Alert> :
-                                null
-                        }
-                        <UserList users={this.state.result} />
-                    </Container>
+                    <Input label="Search" icon={<FontAwesomeIcon icon={faSearch} />} onChange={this.handleSearch
+                    } value={this.state.search} required />
                 </Card>
-
+                <Card>
+                    {
+                        this.state.result.length === 0 ?
+                            "No Results :)" :
+                            <UserList users={this.state.result} />
+                    }
+                </Card>
             </div>
         )
     }
